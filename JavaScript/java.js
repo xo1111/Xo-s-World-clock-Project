ceb33a8be7dba5195e6e5a40ba8aeada152dcd34
xo1111 <162613468+xo1111@users.noreply.github.com>
2024-08-15 20:59:16 +0100

    added all cities link

--- JavaScript/java.js
+++ JavaScript/java.js
@@ -38,7 +38,7 @@ function updateCity (event){
         <div class="time"> ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")} </small></div>
     </div>
   `;
-
+<a href="/">All cities</a>
 }
 
 
